123dc4b1dbd2a5deaa3410a9e0dc540d3d26d085
Hialan Liu <hialan.liu@gmail.com>
2018-07-13 01:09:26 +0800

    add 808

---
+++ medium/DynamicProgramming/808.UniquePaths.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let pathCounts = [];
+    
+    for(let x=0;x<m;x++) {
+        pathCounts[x] = 1;
+    }
+
+    for(let y=1;y<n;y++){
+        for(let x=1;x<m;x++) {
+            pathCounts[x] += pathCounts[x-1];   // pathCount[x] is up value, pathCount[x-1] is left value
+        }
+    }
+
+    return pathCounts[m-1];
+};
+
+console.log(3, uniquePaths(3, 2));
+console.log(28, uniquePaths(7, 3));
