c57e34a7f6a292f106eb07e5d1d06c65e2dcc6f2
Hialan Liu <hialan.liu@gmail.com>
2018-08-01 22:19:20 +0800

    add #815

---
+++ medium/Math/815.HappyNumber.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+class Solution {
+    public boolean isHappy(int n) {
+        ArrayList<Integer> usedNumber = new ArrayList<Integer>();
+        usedNumber.add(n);
+
+        while(true) {
+            int sum = 0;
+            while(n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = (n - digit) / 10;
+            }
+
+            if(sum == 1) {
+                return true;
+            }
+
+            if(usedNumber.contains(sum)) {
+                //return false;
+                break;
+            }
+
+            usedNumber.add(sum);
+            n = sum;
+        }
+
+        return false;
+    }
+}
+
+class HappyNumber {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println("19: " + sol.isHappy(19));
+    }
+}
